223c97f830dc9745df5a1099f8f1ee607b7d165c
Alex <alexiv.04@mail.ru>
2022-10-19 09:21:10 +0700

    Перемещение и дизайн 2

--- app/src/main/java/com/example/test3/MainActivity.java
+++ app/src/main/java/com/example/test3/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 onBackPressed();
-                startService(new Intent(getApplication(), FloatButtonService.class));
+                //startService(new Intent(getApplication(), FloatButtonService.class));
 
 
 
@@ -59,6 +59,7 @@ public class MainActivity extends AppCompatActivity {
         quitDialog.setPositiveButton("Да", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
+                startService(new Intent(getApplication(), FloatButtonService.class));
                 finish();
             }
         });
